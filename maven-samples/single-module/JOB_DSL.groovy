e9b8616efb5b8558aa40b2b145485f825e0572c1
jigglypuff27 <himansh.dit@gmail.com>
2021-03-12 14:37:35 +0530

    Create JOB_DSL.groovy

---
+++ maven-samples/single-module/JOB_DSL.groovy
@@ -0,0 +1,21 @@
+job('First maven project VIA DSL')
+{
+  description ("First project via DSL generated on ${new Date()} ")
+  scm 
+  {
+    git(https://github.com/jigglypuff27/Jenkins_Upgradev3.git,master)
+  }
+  triggers
+  {
+    scm('* * * * *')
+  }
+  steps
+  {
+    maven('clean package','maven-samples/single-module/pom.xml')
+  }
+  publishers
+        {
+          archiveArtifacts '**/*.jar'
+        }
+        
+}  
